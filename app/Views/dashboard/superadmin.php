45ea9acaed2045966e5278fc7c8507874b56a8d0
Fatwa Aulia <fatwaaulia.fy@gmail.com>
2023-05-22 21:01:58 +0700

    memperbarui dashboard superadmin

--- app/Views/dashboard/superadmin.php
+++ app/Views/dashboard/superadmin.php
@@ -9,10 +9,35 @@
             <div class="card mb-3 border-primary" style="border-bottom-width:4px">
                 <div class="card-body text-center py-3">
                     <p class="fw-500 text-secondary d-block mb-2">
-                        <i class="fa-solid fa-user-group"></i> 
-                        Total Pengguna
+                        <i class="fa-solid fa-paper-plane"></i>
+                        Kontribusi Masuk
                     </p>
-                    <h3 class="mb-0"><?= model('Users')->countAll() ?></h3>
+                    <?php
+                    $where = [
+                        'accepted_at' => null,
+                        'status !='   => 'Diterima'
+                    ];
+                    $jumlah_kontribusi = model('Produktif')->where($where)->findAll();
+                    ?>
+                    <h3 class="mb-0"><?= count($jumlah_kontribusi) ?></h3>
+                </div>
+            </div>
+        </div>
+        <div class="col-xxl-3 col-lg-4 col-md-6">
+            <div class="card mb-3 border-success" style="border-bottom-width:4px">
+                <div class="card-body text-center py-3">
+                    <p class="fw-500 text-secondary d-block mb-2">
+                        <i class="fa-solid fa-up-right-from-square"></i>
+                        Data Produktif
+                    </p>
+                    <?php
+                    $where = [
+                        'accepted_at !=' => null,
+                        'status'   => 'Diterima'
+                    ];
+                    $jumlah_produktif = model('Produktif')->where($where)->findAll();
+                    ?>
+                    <h3 class="mb-0"><?= count($jumlah_produktif) ?></h3>
                 </div>
             </div>
         </div>
