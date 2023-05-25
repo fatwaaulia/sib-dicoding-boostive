689eaee1429eeb9b3465379bad1dce8dd665996f
hafizhakmal <hafizhakmal21@gmail.com>
2023-05-25 11:29:05 +0700

    Memperbaiki navigation bar pada mode mobile device agar menjadi dropdown

--- app/Views/landingpage/navbar_footer.php
+++ app/Views/landingpage/navbar_footer.php
@@ -8,28 +8,34 @@
         </div>
         <div class="w-auto">
             <ul class="hidden sm:flex menu menu-horizontal px-1">
-                <li ><a class="text-s">Home</a></li>
+                <li ><a class="text-m">Home</a></li>
                 <li tabindex="0">
-                    <a class="text-s">
+                    <a class="text-m">
                         Produktif
                         <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
                     </a>
                     <ul class="p-2 bg-base-100">
-                        <li><a class="text-s">Submenu 1</a></li>
-                <li><a class="text-s">Submenu 2</a></li>
+                        <li><a class="text-m">Submenu 1</a></li>
+                <li><a class="text-m">Submenu 2</a></li>
             </ul>
         </li>
-        <li><a class="text-s">Tentang Kami</a></li>
+        <li><a class="text-m">Tentang Kami</a></li>
     </ul>
 </div>
 <input type="checkbox" class="peer hidden" id="cek">
 <label for="cek" class="absolute right-4 sm:hidden btn btn-outline py-3 px-4 rounded
  focus:outline-none hover:bg-gray-200 hover:cursor-pointer">☰</label>
-<div class="absolute left-0 top-[60px] w-1/3 bg-white -translate-x-[41rem] border-solid border-black border border-opacity-10
+<div class="rounded-box rounded-l-none absolute left-0 top-[60px] w-1/3 bg-white -translate-x-[41rem] border-solid border-black border border-opacity-10
 peer-checked:translate-x-0 transition duration-500 sm:w-auto sm:static sm:hidden">
     <ul class="max-h-40 font-semibold h-screen p-3 flex flex-col gap-5 sm:h-auto sm:flex-row sm:bg-inherit">
         <li><a>Home</a></li>
-        <li><a>Produktif</a></li>
+        <div class="dropdown" id="dropdown">
+            <label for="dropdown" tabindex="0">Produktif</label>
+                <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a>Submenu 1</a></li>
+                    <li><a>Submenu 2</a></li>
+                 </ul>
+        </div>
         <li><a>Tentang Kami</a></li>
 
     </ul>
