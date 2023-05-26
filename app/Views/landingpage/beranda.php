cffdf180468a484a8d690ea9b2490ac56195fb21
Muhammad Adil Raja Saputra <adilraja19@students.undip.ac.id>
2023-05-26 15:12:18 +0700

    Add Content in Beranda (Not Fix)

--- app/Views/landingpage/beranda.php
+++ app/Views/landingpage/beranda.php
@@ -1,2 +1,58 @@
-<button class="btn rounded-full">Halaman Beranda</button>
+<div class="hero px-6 py-12 md:px-12 text-center lg:text-left" style="background-color: #191D25;">
+    <div class="hero-content text-left text-primary-content">
+        <div class="grid md:grid-cols-2 gap-12 flex items-center">
+            <div class="mt-4 lg:mt-0">
+                <h1 class="text-3xl md:text-4xl xl:text-5xl font-bold tracking-tight mb-8">Yuk, kita lakukan kegiataan produktif!</h1>
+                <p class="">Pilih kegiatan produktif kamu dan dapatkan ilmu baru dengan cara yan menyenangkan.</p>
+            </div>
+            <div class="mb-4 lg:mb-0">
+                <img
+                  src="https://mdbootstrap.com/img/new/ecommerce/vertical/028.jpg"
+                  class="w-full rounded-lg shadow-lg"
+                  alt=""
+                />
+            </div>
+        </div>
+    </div>
+</div>
 
+<section class="px-6 py-12 md:px-12">
+<h1 class="text-3xl md:text-4xl xl:text-5xl font-bold tracking-tight mb-8">Kategori</h1>
+    <div class="grid grid-cols-4 gap-4">
+        <div class="avatar placeholder">
+            <div class="bg-neutral-focus text-neutral-content rounded-full w-24">
+                <span class="text-3xl">K</span>
+            </div>
+        </div> 
+        <div class="avatar placeholder">
+            <div class="bg-neutral-focus text-neutral-content rounded-full w-24">
+                <span class="text-3xl">K</span>
+            </div>
+        </div> 
+        <div class="avatar placeholder">
+            <div class="bg-neutral-focus text-neutral-content rounded-full w-24">
+                <span class="text-3xl">K</span>
+            </div>
+        </div> 
+        <div class="avatar placeholder">
+            <div class="bg-neutral-focus text-neutral-content rounded-full w-24">
+                <span class="text-3xl">K</span>
+            </div>
+        </div> 
+        <div class="avatar placeholder">
+            <div class="bg-neutral-focus text-neutral-content rounded-full w-24">
+                <span class="text-3xl">K</span>
+            </div>
+        </div> 
+        <div class="avatar placeholder">
+            <div class="bg-neutral-focus text-neutral-content rounded-full w-24">
+                <span class="text-3xl">K</span>
+            </div>
+        </div> 
+        <div class="avatar placeholder">
+            <div class="bg-neutral-focus text-neutral-content rounded-full w-24">
+                <span class="text-3xl">K</span>
+            </div>
+        </div> 
+    </div>
+</section>
