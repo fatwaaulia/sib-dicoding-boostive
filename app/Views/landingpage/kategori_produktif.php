8fa875cec7064ade630576ad5ff3d63bf30da89f
Muhammad Adil Raja Saputra <adilraja19@students.undip.ac.id>
2023-05-31 04:46:04 +0700

    Add Card

--- app/Views/landingpage/kategori_produktif.php
+++ app/Views/landingpage/kategori_produktif.php
@@ -1,1 +1,156 @@
-<button class="btn btn-primary" style="margin-top:100px">Halaman Kategori Produktif</button>
+<div class="hero px-6 py-12 md:px-12 text-center lg:text-left" style="background-color: #191D25;">
+    <div class="hero-content text-left text-primary-content">
+        <div class="grid md:grid-cols-2 gap-12 flex items-center">
+            <div class="mt-4 lg:mt-0">
+                <h1 class="text-3xl md:text-4xl xl:text-5xl font-bold tracking-tight">Produktif</h1>
+            </div>
+            <div class="mb-4 lg:mb-0">
+                <img
+                  src="https://mdbootstrap.com/img/new/ecommerce/vertical/028.jpg"
+                  class="w-fit rounded-lg shadow-lg"
+                  alt=""
+                />
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="px-6 py-12 md:px-12">
+    <div class="grid grid-cols-2 max-[425px]:grid-cols-1 lg:grid-cols-3 xl:grid-cols-4 gap-x-10 gap-y-8">
+        <a href="" class="card bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="<?= base_url('assets/img/category-icon/add-cat-icon.png') ?>"/>
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Tambah Produktif Baru</h2>
+                <p class="mb-4">Usulkan produktif baru untuk kategori Belum Terkategori</p>
+                <span class="bg-green-100 text-green-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">Belum Dikategorikan</span>
+                <div class="divider"></div>
+                <div class="flex flex-wrap">
+                    <p class="mr-2">Usulkan</p>
+                    <img src="<?= base_url('assets/img/category-icon/usulkan-icon.png') ?>">
+                </div>
+            </div>
+        </a>
+        <a href="" class="card bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="<?= base_url('assets/img/category-icon/tk-cat-icon.png') ?>"/>
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Tiny Thinkers</h2>
+                <p class="mb-4">Daftar aktivitas/permainan online & offline untuk Computational Thinking anak-anak mulai usia 4 tahun ke atas</p>
+                <span class="bg-yellow-100 text-yellow-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">TK</span>
+                <div class="divider"></div>
+                <div class="flex flex-wrap">
+                    <p class="mr-2">Detail</p>
+                    <img src="<?= base_url('assets/img/category-icon/detail-icon.png') ?>">
+                </div>
+            </div>
+        </a>
+        <a href="" class="card bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="<?= base_url('assets/img/category-icon/sd-cat-icon.png') ?>"/>
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Tiny Thinkers</h2>
+                <p class="mb-4">Daftar aktivitas/permainan online & offline untuk Computational Thinking anak-anak mulai usia 4 tahun ke atas</p>
+                <span class="bg-red-100 text-red-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">SD</span>
+                <div class="divider"></div>
+                <div class="flex flex-wrap">
+                    <p class="mr-2">Detail</p>
+                    <img src="<?= base_url('assets/img/category-icon/detail-icon.png') ?>">
+                </div>
+            </div>
+        </a>
+        <a href="" class="card bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="<?= base_url('assets/img/category-icon/tk-cat-icon.png') ?>"/>
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Tiny Thinkers</h2>
+                <p class="mb-4">Daftar aktivitas/permainan online & offline untuk Computational Thinking anak-anak mulai usia 4 tahun ke atas</p>
+                <span class="bg-yellow-100 text-yellow-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">TK</span>
+                <div class="divider"></div>
+                <div class="flex flex-wrap">
+                    <p class="mr-2">Detail</p>
+                    <img src="<?= base_url('assets/img/category-icon/detail-icon.png') ?>">
+                </div>
+            </div>
+        </a>
+        <a href="" class="card bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="<?= base_url('assets/img/category-icon/sd-cat-icon.png') ?>"/>
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Tiny Thinkers</h2>
+                <p class="mb-4">Daftar aktivitas/permainan online & offline untuk Computational Thinking anak-anak mulai usia 4 tahun ke atas</p>
+                <span class="bg-red-100 text-red-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">SD</span>
+                <div class="divider"></div>
+                <div class="flex flex-wrap">
+                    <p class="mr-2">Detail</p>
+                    <img src="<?= base_url('assets/img/category-icon/detail-icon.png') ?>">
+                </div>
+            </div>
+        </a>
+        <a href="" class="card bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="<?= base_url('assets/img/category-icon/tk-cat-icon.png') ?>"/>
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Tiny Thinkers</h2>
+                <p class="mb-4">Daftar aktivitas/permainan online & offline untuk Computational Thinking anak-anak mulai usia 4 tahun ke atas</p>
+                <span class="bg-yellow-100 text-yellow-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">TK</span>
+                <div class="divider"></div>
+                <div class="flex flex-wrap">
+                    <p class="mr-2">Detail</p>
+                    <img src="<?= base_url('assets/img/category-icon/detail-icon.png') ?>">
+                </div>
+            </div>
+        </a>
+        <a href="" class="card bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="<?= base_url('assets/img/category-icon/sd-cat-icon.png') ?>"/>
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Tiny Thinkers</h2>
+                <p class="mb-4">Daftar aktivitas/permainan online & offline untuk Computational Thinking anak-anak mulai usia 4 tahun ke atas</p>
+                <span class="bg-red-100 text-red-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">SD</span>
+                <div class="divider"></div>
+                <div class="flex flex-wrap">
+                    <p class="mr-2">Detail</p>
+                    <img src="<?= base_url('assets/img/category-icon/detail-icon.png') ?>">
+                </div>
+            </div>
+        </a>
+        <a href="" class="card bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="<?= base_url('assets/img/category-icon/tk-cat-icon.png') ?>"/>
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Tiny Thinkers</h2>
+                <p class="mb-4">Daftar aktivitas/permainan online & offline untuk Computational Thinking anak-anak mulai usia 4 tahun ke atas</p>
+                <span class="bg-yellow-100 text-yellow-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">TK</span>
+                <div class="divider"></div>
+                <div class="flex flex-wrap">
+                    <p class="mr-2">Detail</p>
+                    <img src="<?= base_url('assets/img/category-icon/detail-icon.png') ?>">
+                </div>
+            </div>
+        </a>
+        <a href="" class="card bg-base-100 shadow-xl">
+            <figure class="px-10 pt-10">
+                <img src="<?= base_url('assets/img/category-icon/sd-cat-icon.png') ?>"/>
+            </figure>
+            <div class="card-body items-center text-center">
+                <h2 class="card-title">Tiny Thinkers</h2>
+                <p class="mb-4">Daftar aktivitas/permainan online & offline untuk Computational Thinking anak-anak mulai usia 4 tahun ke atas</p>
+                <span class="bg-red-100 text-red-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">SD</span>
+                <div class="divider"></div>
+                <div class="flex flex-wrap">
+                    <p class="mr-2">Detail</p>
+                    <img src="<?= base_url('assets/img/category-icon/detail-icon.png') ?>">
+                </div>
+            </div>
+        </a>
+    </div>
+</section>
