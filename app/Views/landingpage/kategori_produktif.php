ce731f3aae301fa06bf027a08ddad8c5dc9f9500
Fatwa Aulia <fatwaaulia.fy@gmail.com>
2023-06-02 17:21:50 +0700

    integrasi data produktif dengan backend

--- app/Views/landingpage/kategori_produktif.php
+++ app/Views/landingpage/kategori_produktif.php
@@ -3,7 +3,14 @@
         <div class="hero-content text-left text-primary-content">
         <div class="grid md:grid-cols-2 gap-12 flex items-center justify-center mb-4">
             <div class="mt-4 lg:mt-0 md:mb-8 xl:ml-40">
-                <h1 class="text-3xl md:text-3xl xl:text-4xl font-bold tracking-tight text-center">Sekolah Menengah Atas (SMA)</h1>
+                <?php 
+                $segment_2 = str_replace('%20', ' ', service('uri')->getSegment(2));
+                $kategori = model('KategoriProduktif')->where('nama', $segment_2)->first(); 
+                ?>
+                <h1 class="text-3xl md:text-3xl xl:text-4xl font-bold tracking-tight text-center">
+                    <?= $kategori['deskripsi'] ?>
+                    (<?= $kategori['nama'] ?>)
+                </h1>
             </div>
             <div class="mb-4 lg:mb-0">
                 <img
@@ -25,7 +32,7 @@
             <div class="card-body items-center text-center">
                 <h2 class="card-title">Tambah Produktif Baru</h2>
                 <p class="mb-0">Usulkan produktif baru untuk kategori Belum Terkategori</p>
-                <span class="bg-green-100 text-green-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">Belum Dikategorikan</span>
+                <span class="bg-red-100 text-red-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">Belum Dikategorikan</span>
                 <div class="divider"></div>
                 <div class="flex flex-wrap">
                     <a class='flex'href=""><p class="mr-2">Usulkan</p>
@@ -34,24 +41,44 @@
                 </div>
             </div>
         </div>
-        <?php for ($i=0; $i <10 ; $i++) : ?>
+        <?php
+        $data_produktif_by_kategori = model('Produktif')->where('id_kategori', $kategori['id'])->findAll();
+        foreach ($data_produktif_by_kategori as $v) :
+        ?>
             <div class="card bg-base-100 shadow-xl border-solid border-t-0 border-x-0 border-2 border-gray-200">
             <figure class='max-h-52'>
-                <img src="https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=455&q=80"/>
+                <?php 
+                if ($v['img']) {
+                    $img = base_url('assets/img/produktif/') . $v['img'];
+                } else {
+                    $img = base_url('assets/img/default.png');
+                }
+                ?>
+                <img src="<?= $img ?>"/>
             </figure>
             <div class="card-body items-center text-center">
-                <h2 class="card-title">Tiny Thinkers</h2>
-                <p class="mb-0">Daftar aktivitas/permainan online & offline untuk Computational Thinking anak-anak mulai usia 4 tahun ke atas</p>
-                <span class="bg-red-100 text-red-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">SD</span>
+                <style>
+                .lc-2 {
+                    text-overflow: ellipsis;
+                    overflow: hidden;
+                    display: -webkit-box;
+                    -webkit-box-orient: vertical;
+                    -webkit-line-clamp: 2 !important;
+                    max-height: 50px;
+                }   
+                </style>
+                <h2 class="card-title"><?= $v['nama'] ?></h2>
+                <p class="mb-0 lc-2"><?= $v['deskripsi'] ?></p>
+                <span class="bg-green-100 text-green-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full"><?= $kategori['nama'] ?></span>
                 <div class="divider"></div>
                 <div class="flex flex-wrap">
-                    <a class='flex'href=""><p class="mr-2">Detail</p>
+                    <a class='flex'href="<?= base_url('produktif/') . $kategori['nama'] . '/' . $v['slug'] ?>"><p class="mr-2">Detail</p>
                         <img src="<?= base_url('assets/img/category-icon/detail-icon.png') ?>">
                     </a>
                 </div>
             </div>
         </div>
-        <?php endfor;?>
+        <?php endforeach; ?>
     </div>
 </section>
 </div>
