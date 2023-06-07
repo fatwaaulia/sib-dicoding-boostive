5239682060a2352421e71552f3a7e21406bd9d9e
Fatwa Aulia <fatwaaulia.fy@gmail.com>
2023-06-07 18:06:52 +0700

    set database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'ci4',
+        'database' => 'sib_dicoding_boostive',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
