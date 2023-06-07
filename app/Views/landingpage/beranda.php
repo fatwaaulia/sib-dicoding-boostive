653db3c16d362291572908acb56e9eee9323fe27
Muhammad Adil Raja Saputra <adilraja19@students.undip.ac.id>
2023-06-08 05:10:42 +0700

    Add New Content in Beranda

--- app/Views/landingpage/beranda.php
+++ app/Views/landingpage/beranda.php
@@ -2,7 +2,7 @@
     <div class="hero-content text-left text-primary-content">
         <div class="grid md:grid-cols-2 gap-12 flex items-center">
             <div class="mt-4 lg:mt-0">
-                <h1 class="text-3xl md:text-4xl xl:text-5xl font-bold tracking-tight mb-8">Yuk, kita lakukan kegiataan produktif!</h1>
+                <h1 class="text-3xl md:text-4xl xl:text-5xl font-bold tracking-tight mb-8">Yuk, berkenalan dengan BOOSTIVE dan salurkan Ide Produktif kamu</h1>
                 <p class="">Pilih kegiatan produktif kamu dan dapatkan ilmu baru dengan cara yan menyenangkan.</p>
             </div>
             <div class="mb-4 lg:mb-0">
@@ -15,4 +15,30 @@
         </div>
     </div>
 </div>
+
+<section class="px-6 pb-12 md:px-12">
+    <h1 class="text-center text-2xl md:text-3xl font-bold text-red-800 tracking-tight mt-6 mb-8">Tahukah Kamu Manfaat Menjadi Orang Yang Produktif???</h1>
+    <ol class="px-4 list-decimal grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-8">
+        <li class="text-lg font-bold">
+            Pintar Mengatur Waktu
+            <p class="text-base font-normal text-justify">Orang yang produktif cenderung mampu menyelesaikan pekerjaan tepat pada waktunya. Ini karena mereka konsisten menjaga fokusnya ketika melakukan sesuatu, sehingga tidak mudah terdistraksi. Maka tak heran, apabila orang yang produktif senantiasa lihai dalam mengelola waktu yang mereka miliki. Alhasil, tak akan ada lagi waktu yang terbuang begitu saja.</p>
+        </li>
+        <li class="text-lg font-bold">
+            Memperoleh lebih banyak kesempatan
+            <p class="text-base font-normal text-justify">Ketika seseorang sudah terbiasa menjalani hidup yang produktif, biasanya dia bakal sering mendapatkan kesempatan lebih untuk mencoba banyak hal lain. Inilah alasan yang membuat hidup tidak akan pernah terasa membosankan, karena selalu ada saja pilihan yang menarik untuk dikerjakan. Lalu, jika beruntung, peluang tersebut bisa menjadi potensi baru yang mungkin akan sangat berguna untuk ke depannya.</p>
+        </li>
+        <li class="text-lg font-bold">
+            Terhindar dari stres
+            <p class="text-base font-normal text-justify">Stres dapat dipicu oleh beberapa faktor, salah satunya adalah overthinking, atau berpikir secara berlebihan. Sementara itu, kebahagiaan dan rasa puas timbul dari pikiran kita yang terpusat. Itulah sebabnya orang yang produktif imun terhadap serangan pikiran negatif, karena mereka selalu mencurahkan fokus pada satu tugas sebelum beralih ke tugas berikutnya. Jadi, sebisa mungkin jangan pernah membiarkan diri kita ini menganggur, tanpa melakukan sesuatu yang berarti. Sebab, saat kita diam terlalu lama, berbagai kekhawatiran bisa tiba-tiba mengusik pikiran.</p>
+        </li>
+        <li class="text-lg font-bold">
+            Kemampuan meningkat secara signifikan
+            <p class="text-base font-normal text-justify">Jika kita ingin semakin mahir dalam suatu bidang tertentu, maka kita perlu meningkatkan produktivitas diri kita. Lalu, melakukan hal itu secara berulang-ulang, terjadwal, dan kontinu, tanpa mengenal kata bosan. Dengan semua upaya tersebut, maka otomatis kemampuan kita akan mengalami perkembangan sesuai dengan yang diharapkan. Inilah untungnya menjadi orang yang selalu berusaha produktif.</p>
+        </li>
+        <li class="text-lg font-bold">
+            Waktu luang yang tersedia lebih banyak
+            <p class="text-base font-normal text-justify">Orang yang benar-benar produktif tahu kapan waktunya untuk rehat dan menikmati waktu luang. Setelah mereka mampu mencapai target, mereka akan memanjakan diri dengan beristirahat sejenak atau melakukan aktivitas lain yang menyenangkan. Dan, untungnya lagi, waktu luang yang mereka miliki biasanya jauh lebih panjang, karena pekerjaan mereka yang sebelumnya telah diselesaikan jauh dari deadline.</p>
+        </li>
+    </ol>
+</section>
 
