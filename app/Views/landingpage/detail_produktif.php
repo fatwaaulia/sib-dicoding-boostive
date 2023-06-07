3229d007a98858097767066a1c22ba7fa727dca2
Muhammad Adil Raja Saputra <adilraja19@students.undip.ac.id>
2023-06-08 05:11:08 +0700

    Add Detail Produktif Page

--- app/Views/landingpage/detail_produktif.php
+++ app/Views/landingpage/detail_produktif.php
@@ -1,1 +1,22 @@
-<button class="btn btn-primary" style="margin-top: 100px">Halaman Detail</button>
+<section class="px-6 pb-12 md:px-12">
+    <div class="grid gap-4 place-items-center" style="margin-top: 100px">
+        <div class="bg-red-100 text-red-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">Kategori SD</div>
+        <h1 class="text-center text-2xl md:text-3xl font-semibold tracking-tight">Tiny Thingkers</h1>
+        <span class="h-1.5 w-96 bg-red-800 rounded-full lg:w-1/3"></span>
+        <img
+            src="https://tecdn.b-cdn.net/img/new/slides/041.jpg"
+            class="object-contain md:object-scale-down xl:max-w-6xl"
+            alt="..." />
+        <div class="">
+            <h1 class="text-lg font-semibold">Deskripsi</h1>
+            <p class="text-justify">Check out these games that have been curated by the experts over at Coding Lab! While we have included the recommended ages for each activity, every child's progression will be different. Feel free to explore their abilities and push boundaries. Multiple levels: Some games have up to 3 levels. We recommend starting from level 1.</p>
+
+            <h1 class="text-lg font-semibold mt-4">Link Kegiatan :</h1>
+            <a href="https://www.tinythinkers.org/games">
+                <p class="text-justify">https://www.tinythinkers.org/games</p>
+            </a>
+
+            <button class="btn mt-6">Beri Masukan</button>
+        </div>
+    </div>
+</section>
