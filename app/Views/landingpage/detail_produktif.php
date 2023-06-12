1d9c813f8395138224ec56d0e783d87eadbe9f6e
hafizhakmal <hafizhakmal21@gmail.com>
2023-06-12 14:39:41 +0700

    Merapikan tampilan page detail item produktif

--- app/Views/landingpage/detail_produktif.php
+++ app/Views/landingpage/detail_produktif.php
@@ -1,5 +1,6 @@
+<div class="h-20"></div>
 <section class="px-6 pb-12 md:px-12">
-    <div class="grid gap-4 place-items-center" style="margin-top: 100px">
+    <div class="grid gap-4 place-items-center">
         <div class="bg-red-100 text-red-800 text-md font-medium mr-2 px-2.5 py-0.5 rounded-full">Kategori <?= $kategori ?></div>
         <h1 class="text-center text-2xl md:text-3xl font-semibold tracking-tight"><?= $produktif['nama'] ?></h1>
         <span class="h-1.5 w-96 bg-red-800 rounded-full lg:w-1/3"></span>
@@ -14,7 +15,7 @@
             src="<?= $img ?>"
             class="object-contain md:object-scale-down xl:max-w-6xl"
             alt="..." />
-        <div class="">
+        <div class=xl:max-w-6xl>
             <h1 class="text-lg font-semibold">Deskripsi</h1>
             <p class="text-justify"><?= $produktif['deskripsi'] ?></p>
 
