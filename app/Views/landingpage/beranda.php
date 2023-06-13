5bdae09c0550d1fbb60ff751c2ca628bb6287757
Muhammad Adil Raja Saputra <adilraja19@students.undip.ac.id>
2023-06-14 00:25:41 +0700

    Perbaikan Card dan Penambahan Tips

--- app/Views/landingpage/beranda.php
+++ app/Views/landingpage/beranda.php
@@ -16,30 +16,9 @@
     </div>
 </div>
 
-<section class="w-full flex flex-col flex-wrap items-center gap-32 mt-10 px-6 pb-12 md:px-12">
-    <div class="max-w-[1200px] md:grid md:grid-cols-2 md:gap-5">
-        <div class="flex justify-center">
-            <img src="<?= base_url('assets/img/beranda/tips-1.png') ?>" alt="">
-        </div>
-        <div class="flex flex-col justify-center mt-6">
-            <h2 class="text-lg font-semibold">Judul</h2>
-            <p class="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium officia, eligendi dolores impedit temporibus, laborum consequatur quidem nihil aut beatae assumenda omnis! Unde ab quis earum voluptatibus cumque amet eligendi.</p>
-        </div>
-    </div>
-    <div class="max-w-[1200px] md:grid md:grid-cols-2 md:gap-5">
-        <div class="flex justify-center md:col-start-2">
-            <img src="<?= base_url('assets/img/beranda/tips-1.png') ?>" alt="">
-        </div>
-        <div class="flex flex-col justify-center mt-6 md:col-start-1 md:row-start-1">
-            <h2 class="text-lg font-semibold">Judul</h2>
-            <p class="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium officia, eligendi dolores impedit temporibus, laborum consequatur quidem nihil aut beatae assumenda omnis! Unde ab quis earum voluptatibus cumque amet eligendi.</p>
-        </div>
-    </div>
-</section>
-
 <section class="px-6 pb-12 md:px-12">
     <h1 class="text-center text-2xl md:text-3xl font-semibold tracking-tight mt-8 mb-8">Fitur Boostive</h1>
-    <div class="grid grid-cols-1 md:grid-cols-2 sm:place-items-center gap-x-5 gap-y-8">
+    <div class="grid grid-cols-1 min-[900px]:grid-cols-2 place-items-center gap-x-5 gap-y-8">
         <div class="card w-96 bg-base-100 shadow-xl">
             <div class="card-body rounded-tl-3xl rounded-br-3xl bg-blue-200">
                 <h1 class="card-title text-blue-800">Produktivitas</h1>
@@ -47,7 +26,7 @@
             <div class="card-body items-center text-center">
                 <p class="text-justify">Banyak tools produktif yang tersedia, gunakanlah waktu luang anda untuk melakukan hal-hal positif dan jadilah produktif!</p>
                 <div class="card-actions">
-                    <button class="btn btn-info rounded-full">Selengkapnya</button>
+                    <a href="<?= base_url('produktif') ?>"><button class="btn btn-info rounded-full">Selengkapnya</button></a>
                 </div>
             </div>
         </div>
@@ -59,7 +38,7 @@
                 <div class="card-body items-center text-center">
                     <p class="text-justify">Kontribusi data disediakan untuk anda dapat ikut memberikan rekomendasi tools produktif pada boostive</p>
                     <div class="card-actions">
-                        <button class="btn btn-error rounded-full">Selengkapnya</button>
+                        <a href="<?= base_url('formulir-kontribusi') ?>"><button class="btn btn-error rounded-full">Selengkapnya</button></a>
                     </div>
                 </div>
             </div>
@@ -67,31 +46,51 @@
     </div>
 </section>
 
-
-
-<section class="px-6 pb-12 md:px-12">
-    <h1 class="text-center text-2xl md:text-3xl font-bold text-red-800 tracking-tight mt-2 mb-8">Tahukah Kamu Manfaat Menjadi Orang Yang Produktif???</h1>
-    <ol class="px-4 list-decimal grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-8">
-        <li class="text-lg font-bold">
-            Pintar Mengatur Waktu
-            <p class="text-base font-normal text-justify">Orang yang produktif cenderung mampu menyelesaikan pekerjaan tepat pada waktunya. Ini karena mereka konsisten menjaga fokusnya ketika melakukan sesuatu, sehingga tidak mudah terdistraksi. Maka tak heran, apabila orang yang produktif senantiasa lihai dalam mengelola waktu yang mereka miliki. Alhasil, tak akan ada lagi waktu yang terbuang begitu saja.</p>
-        </li>
-        <li class="text-lg font-bold">
-            Memperoleh lebih banyak kesempatan
-            <p class="text-base font-normal text-justify">Ketika seseorang sudah terbiasa menjalani hidup yang produktif, biasanya dia bakal sering mendapatkan kesempatan lebih untuk mencoba banyak hal lain. Inilah alasan yang membuat hidup tidak akan pernah terasa membosankan, karena selalu ada saja pilihan yang menarik untuk dikerjakan. Lalu, jika beruntung, peluang tersebut bisa menjadi potensi baru yang mungkin akan sangat berguna untuk ke depannya.</p>
-        </li>
-        <li class="text-lg font-bold">
-            Terhindar dari stres
-            <p class="text-base font-normal text-justify">Stres dapat dipicu oleh beberapa faktor, salah satunya adalah overthinking, atau berpikir secara berlebihan. Sementara itu, kebahagiaan dan rasa puas timbul dari pikiran kita yang terpusat. Itulah sebabnya orang yang produktif imun terhadap serangan pikiran negatif, karena mereka selalu mencurahkan fokus pada satu tugas sebelum beralih ke tugas berikutnya. Jadi, sebisa mungkin jangan pernah membiarkan diri kita ini menganggur, tanpa melakukan sesuatu yang berarti. Sebab, saat kita diam terlalu lama, berbagai kekhawatiran bisa tiba-tiba mengusik pikiran.</p>
-        </li>
-        <li class="text-lg font-bold">
-            Kemampuan meningkat secara signifikan
-            <p class="text-base font-normal text-justify">Jika kita ingin semakin mahir dalam suatu bidang tertentu, maka kita perlu meningkatkan produktivitas diri kita. Lalu, melakukan hal itu secara berulang-ulang, terjadwal, dan kontinu, tanpa mengenal kata bosan. Dengan semua upaya tersebut, maka otomatis kemampuan kita akan mengalami perkembangan sesuai dengan yang diharapkan. Inilah untungnya menjadi orang yang selalu berusaha produktif.</p>
-        </li>
-        <li class="text-lg font-bold">
-            Waktu luang yang tersedia lebih banyak
-            <p class="text-base font-normal text-justify">Orang yang benar-benar produktif tahu kapan waktunya untuk rehat dan menikmati waktu luang. Setelah mereka mampu mencapai target, mereka akan memanjakan diri dengan beristirahat sejenak atau melakukan aktivitas lain yang menyenangkan. Dan, untungnya lagi, waktu luang yang mereka miliki biasanya jauh lebih panjang, karena pekerjaan mereka yang sebelumnya telah diselesaikan jauh dari deadline.</p>
-        </li>
-    </ol>
+<section class="w-full flex flex-col flex-wrap items-center gap-8 px-6 pb-12 md:px-12">
+    <h1 class="text-center text-2xl md:text-3xl font-bold text-red-800 tracking-tight mt-2 mb-4">Tahukah Kamu Manfaat Menjadi Orang Yang Produktif?</h1>
+    <div class="max-w-[1200px] md:grid md:grid-cols-2 md:gap-5">
+        <div class="flex justify-center">
+            <img src="<?= base_url('assets/img/beranda/tips-1.png') ?>" alt="">
+        </div>
+        <div class="flex flex-col justify-center mt-6">
+            <h2 class="text-lg font-bold">Pintar Mengatur Waktu</h2>
+            <p class="mt-2 text-base font-normal text-justify">Orang yang produktif cenderung mampu menyelesaikan pekerjaan tepat pada waktunya. Ini karena mereka konsisten menjaga fokusnya ketika melakukan sesuatu, sehingga tidak mudah terdistraksi. Maka tak heran, apabila orang yang produktif senantiasa lihai dalam mengelola waktu yang mereka miliki. Alhasil, tak akan ada lagi waktu yang terbuang begitu saja.</p>
+        </div>
+    </div>
+    <div class="max-w-[1200px] md:grid md:grid-cols-2 md:gap-5">
+        <div class="flex justify-center md:col-start-2">
+            <img src="<?= base_url('assets/img/beranda/tips-2.png') ?>" alt="">
+        </div>
+        <div class="flex flex-col justify-center mt-6 md:col-start-1 md:row-start-1">
+            <h2 class="text-lg font-bold">Memperoleh lebih banyak kesempatan</h2>
+            <p class="mt-2 text-base font-normal text-justify">Ketika seseorang sudah terbiasa menjalani hidup yang produktif, biasanya dia bakal sering mendapatkan kesempatan lebih untuk mencoba banyak hal lain. Inilah alasan yang membuat hidup tidak akan pernah terasa membosankan, karena selalu ada saja pilihan yang menarik untuk dikerjakan. Lalu, jika beruntung, peluang tersebut bisa menjadi potensi baru yang mungkin akan sangat berguna untuk ke depannya.</p>
+        </div>
+    </div>
+    <div class="max-w-[1200px] md:grid md:grid-cols-2 md:gap-5">
+        <div class="flex justify-center">
+            <img src="<?= base_url('assets/img/beranda/tips-3.png') ?>" alt="">
+        </div>
+        <div class="flex flex-col justify-center mt-6">
+            <h2 class="text-lg font-bold">Terhindar dari stres</h2>
+            <p class="mt-2 text-base font-normal text-justify">Stres dapat dipicu oleh beberapa faktor, salah satunya adalah overthinking, atau berpikir secara berlebihan. Sementara itu, kebahagiaan dan rasa puas timbul dari pikiran kita yang terpusat. Itulah sebabnya orang yang produktif imun terhadap serangan pikiran negatif, karena mereka selalu mencurahkan fokus pada satu tugas sebelum beralih ke tugas berikutnya. Jadi, sebisa mungkin jangan pernah membiarkan diri kita ini menganggur, tanpa melakukan sesuatu yang berarti. Sebab, saat kita diam terlalu lama, berbagai kekhawatiran bisa tiba-tiba mengusik pikiran.</p>
+        </div>
+    </div>
+    <div class="max-w-[1200px] md:grid md:grid-cols-2 md:gap-5">
+        <div class="flex justify-center md:col-start-2">
+            <img src="<?= base_url('assets/img/beranda/tips-4.png') ?>" alt="">
+        </div>
+        <div class="flex flex-col justify-center mt-6 md:col-start-1 md:row-start-1">
+            <h2 class="text-lg font-bold">Kemampuan meningkat secara signifikan</h2>
+            <p class="mt-2 text-base font-normal text-justify">Jika kita ingin semakin mahir dalam suatu bidang tertentu, maka kita perlu meningkatkan produktivitas diri kita. Lalu, melakukan hal itu secara berulang-ulang, terjadwal, dan kontinu, tanpa mengenal kata bosan. Dengan semua upaya tersebut, maka otomatis kemampuan kita akan mengalami perkembangan sesuai dengan yang diharapkan. Inilah untungnya menjadi orang yang selalu berusaha produktif.</p>
+        </div>
+    </div>
+    <div class="max-w-[1200px] md:grid md:grid-cols-2 md:gap-5">
+        <div class="flex justify-center">
+            <img src="<?= base_url('assets/img/beranda/tips-5.png') ?>" alt="">
+        </div>
+        <div class="flex flex-col justify-center mt-6">
+            <h2 class="text-lg font-bold">Waktu luang yang tersedia lebih banyak</h2>
+            <p class="mt-2 text-base font-normal text-justify">Orang yang benar-benar produktif tahu kapan waktunya untuk rehat dan menikmati waktu luang. Setelah mereka mampu mencapai target, mereka akan memanjakan diri dengan beristirahat sejenak atau melakukan aktivitas lain yang menyenangkan. Dan, untungnya lagi, waktu luang yang mereka miliki biasanya jauh lebih panjang, karena pekerjaan mereka yang sebelumnya telah diselesaikan jauh dari deadline.</p>
+        </div>
+    </div>
 </section>
-
